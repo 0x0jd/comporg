e70b35464bbc4f897390b2695a2dd3687d3f6256
0x0jd <theimmortaljd@protonmail.com>
2021-03-30 18:45:06 -0400

    mod8

---
+++ MOD8/prog1.s
@@ -0,0 +1,59 @@
+/* Preamble
+# Title: ARM Assembly Template
+# Purpose: MOD8 Program 1 Main
+# Author: Justin E
+# Edit Date: 29MAR2021
+# JHU - COMP ORG - EN.605.204.8VL.SP21 Computer Organization
+End Preamble */
+
+.global main
+    .text
+
+main:
+#Main Prologue
+sub sp, sp, #4
+str lr, [sp, #0]
+
+#User Input prompt1 (m value)
+ldr r0, =prompt1
+bl printf
+
+#Read User Input m into num1
+ldr r0, =input
+ldr r1, =num1
+bl scanf
+
+#User Input prompt2 (hr value)
+ldr r0, =prompt2
+bl printf
+
+#Read User Input hr into num2
+ldr r0, =input
+ldr r1, =num2
+bl scanf
+
+# load miles into r0 and hours into r1
+ldr r0, =num1
+ldr r0, [r0, #0]
+ldr r1, =num2
+ldr r1, [r1, #0]
+#branch and link to function
+bl mph
+
+
+mov r1, r0
+ldr r0, =output
+bl printf
+
+#Main Epilogue
+mov r0, #0
+ldr lr, [sp, #0]
+add sp, sp, #4
+mov pc, lr
+
+.data
+    prompt1: .asciz "Please enter an integer for miles: \n"
+    prompt2: .asciz "Please enter an integer for hour(s): \n"
+    input: .asciz "%d"
+    num1: .word 0
+    num2: .word 0
